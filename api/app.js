e00cb7ebf1e1d496f446bf37e7d0e43563662cc7
danielmdavis <danis1911@gmail.com>
2021-02-05 18:51:09 -0500

    api pulled in and displayed in ui

--- api/app.js
+++ api/app.js
@@ -16,6 +16,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const basePathToData = path.join(__dirname, './');
 
 const getJsonData = function (basePathToData,filename) {
