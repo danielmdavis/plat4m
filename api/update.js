6cc3e52c55dbb2342082bd3493c2deb8d057ed86
danielmdavis <danis1911@gmail.com>
2021-02-11 17:20:25 -0500

    proof of upvoting. jankiness of outcome indicates that the data.json passthru has reached its absolute breaking point

--- api/update.js
+++ api/update.js
@@ -3,7 +3,6 @@ var fs = require('fs');
 
 
 const url = "mongodb+srv://admin:Eternity1%21%21@cluster0.kehb2.mongodb.net/plat4m?retryWrites=true&w=majority";
-// const options = { useUnifiedTopology: true }, { useNewUrlParser: true }, { connectTimeoutMS: 30000 }, { keepAlive: 1}
 const client = new MongoClient(url, { useUnifiedTopology: true }, { useNewUrlParser: true }, { connectTimeoutMS: 30000 }, { keepAlive: 1});
 
  async function run(payload) {
@@ -12,17 +11,28 @@ const client = new MongoClient(url, { useUnifiedTopology: true }, { useNewUrlPar
          console.log("Connected correctly to server")
          const db = client.db("cluster0")
 
-         console.log(payload)
-         payload = JSON.parse(payload)
 
-         const p = await db.collection("test").insertOne(payload)
-         console.log(p);
+
          const returns = await db.collection("test").find()
 
          let data = []
 
          //this may be losing inner addendum objs
          await returns.forEach(each => data.push(each))
+
+
+         payload = JSON.parse(payload)
+         if (!('update' in payload)) {
+           const p = await db.collection("test").insertOne(payload)
+         } else {
+           const id = (Object.values(payload)[0])
+           console.log(id);
+           await db.collection("test").update(
+             { "id": id },
+             { $inc: {  "ups": 1 } }
+           )
+         }
+
          data = JSON.stringify(data)
 
          fs.writeFile('./data.json', data, err => {
