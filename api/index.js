06eb6e1bbd172d9d4ec93ce69e54a887f29af966
danielmdavis <danis1911@gmail.com>
2022-11-27 15:32:30 -0500

    express api w dummy data

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+const data = require('./data');
+
 app.get('/', (req, res) => {
-    res.send('foo')
+    res.send(data)
 })
 
 app.listen(3001, () => {
