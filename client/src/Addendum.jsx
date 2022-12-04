e03f8f2a24b71d332793e3d7d62b93d6a1bca1ab
danielmdavis <danis1911@gmail.com>
2022-12-04 14:43:45 -0500

    long awaited addendum vote counter

--- client/src/Addendum.jsx
+++ client/src/Addendum.jsx
@@ -6,23 +6,48 @@ export default function Addendum(props) {
 
   return (
     <div className='addendum'>
-      <Button
-        variant="contained"
-        color="secondary"
+      <div style={{ flexDirection: 'column' }}>
+        <Button
+          variant="contained"
+          color="secondary"
+          style={{ marginTop: '-6px' }}
         >
-        No
-      </Button>
+          No
+        </Button>
+        <br />
+        <span style={{ cursor: 'not-allowed', pointerEvents: 'none' }}>
+          <Button
+            style={{ fontSize: '16px', maxHeight: '36.5px', marginTop: '2px' }}
+            variant="outlined"
+            color="secondary"
+          >
+            2
+          </Button>
+        </span>
+      </div>
       <Card className='addendum-inner' style={{ backgroundColor: props.predicate ? '#F5C4D5' : 'rgb(246, 246, 246)' }}>
         {props.claim}
-
       </Card>
-      <Button
-        style={{}}
-        variant="contained"
-        color="primary"
+      <div style={{ flexDirection: 'column' }}>
+        <Button
+          style={{}}
+          variant="contained"
+          color="primary"
+          style={{ marginTop: '-6px' }}
         >
-        Yes
-      </Button>
+          Yes
+        </Button>
+        <br />
+        <span style={{ cursor: 'not-allowed', pointerEvents: 'none' }}>
+          <Button
+            style={{ fontSize: '16px', maxHeight: '36.5px', marginTop: '2px' }}
+            variant="outlined"
+            color="primary"
+          >
+            3
+          </Button>
+        </span>
+      </div>
     </div>
   );
 }
