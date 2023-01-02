87b06bd94ba8deb1bad1af2fd54e15b6d084f316
danielmdavis <danis1911@gmail.com>
2023-01-02 14:03:35 -0500

    amplify yml refresh

--- api/app.js
+++ api/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 
-const app = express();
+const app = express(); 
 
 const middleware = require('./middlewareMongo');
 const data = require('./data');
