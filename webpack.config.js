34751cc6b62041efde7cfd7d43edd683934e25a5
danielmdavis <danis1911@gmail.com>
2023-01-19 11:32:01 -0500

    api hack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
-const path = require('path')
-
-  const config = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
-  },
-}
-
-
-module.exports = config;
+module.exports = {
+    entry: "./appMongo.js",
+    resolve: {
+      fallback: {
+        "url": require.resolve("url/"),
+        "path": require.resolve("path-browserify"),
+        "stream": require.resolve("stream-browserify"),
+        "buffer": require.resolve("buffer/"),
+        "buffer": false,
+        "url": false,
+        "path": false,
+        "querystring": require.resolve("querystring-es3"),
+        "http": require.resolve("stream-http"),
+        "http": false,
+        "crypto": false,
+        "zlib": false,
+        "querystring": false
+      }
+    }
+  };
